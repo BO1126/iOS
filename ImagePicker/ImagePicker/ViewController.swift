96407814beba9fb7e7b65f5b44c103316e04813e
jungwoo Lee <b5nnnal88@gmail.com>
2021-04-17 15:38:01 +0900

    ios-0417

---
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by 이정우 on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let picker : UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
+    @IBOutlet weak var imageView : UIImageView!
+    
+    @IBAction func touchUpSelectImageButton(_ sender : UIButton){
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let originalImage : UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            self.imageView.image = originalImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfomation.swift
+//  ViewTransition
+//
+//  Created by 이정우 on 2021/04/17.
+//
+
+import Foundation
+
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var age: String?
+    
+}
